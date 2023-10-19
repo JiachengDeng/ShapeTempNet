8d8a1df88416500c56937964f4f87d6c66b424d7
jiachengdeng <jiachengdeng7@gmail.com>
2023-10-19 19:31:09 +0800

    update

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
-CUDA_VISIBLE_DEVICES=2 python train_point_corr.py \
+CUDA_VISIBLE_DEVICES=6 python train_point_corr.py \
 --dataset_name tosca \
 --train_batch_size 4 \
 --do_train false \
 --optimizer adam \
---arch LuckPointCorr_fps_mae \
+--arch ImplicitTemplatePointCorr \
 --ckpt_period 5 \
 --train_val_split 0.8 \
 --layer_list ssss \
 --steplr2 \
 --d_feedforward 512 \
---resume_from_checkpoint "/ssd/djc/PointCorrespondence/DPC_mae/output/shape_corr/LuckPointCorr/arch_LuckPointCorr_select_mae/dataset_name_tosca/latent_dim_768/23_09:11:05:41/epoch=499.ckpt" \
+--resume_from_checkpoint "/ssd/djc/PointCorrespondence/ShapeTempNet/output/shape_corr/ImplicitTemplatePointCorr/arch_ImplicitTemplatePointCorr/dataset_name_tosca/latent_dim_768/18_10:15:53:52/epoch=299.ckpt" \
 --test_on_tosca \
+--simi_metric embed \
+--template_div_lambda 0.0 \
 # --matrix_post_process offline_ot \
 # --compute_perm_loss \
 # --steplr2 \
